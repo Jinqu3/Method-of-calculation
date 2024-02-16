379e78332836bb814dac0cbe1d1954cc427fde53
Jinqu3 <132920100+Jinqu3@users.noreply.github.com>
2024-02-16 14:10:06 +0400

    1.2
    Added Upgraded Gauss Method

--- lab2/main.py
+++ lab2/main.py
@@ -6,22 +6,59 @@ def GaussMethod(M:list,b:list,n:int)->list:
             for j in range(k, n):
                 if k == j:
                     M[i][j] = 0
+                    continue
                 M[i][j] -= M[k][j] * coefficient
             b[i] -= b[k] * coefficient
 
     return answer(M,b,n)
-def answer(M:list,b:list,n:int)->list:
+
+def UpgradedGaussMethod(M:list,b:list,n:int)->list:
+    order = [i for i in range(n)]
+
+    for k in range(0, n):
+        column = k
+        row = k
+        max_el = M[k][k]
+
+        #нахождение максимального элемента в матрице
+        for i in range(k,n):
+            for j in range(k,n):
+                if M[i][j] > max_el:
+                    row = i
+                    column = j
+                    max_el = M[i][j]
+
+        if k != row and k != column:
+            # замена строк
+            M[k],M[row] = M[row],M[k]
+            b[k],b[row] = b[row],b[k]
+            # замена столбцов
+            for i in range(n):
+                M[i][column],M[i][k] = M[i][k],M[i][column]
+            order[k], order[column] = order[column], order[k]
+
+        for i in range(k + 1, n):
+            coefficient = M[i][k] / M[k][k]
+            for j in range(k, n):
+                if k == j:
+                    M[i][j] = 0
+                    continue
+                M[i][j] -= M[k][j] * coefficient
+            b[i] -= b[k] * coefficient
+
+    return answer(M, b,order, n)
+def answer(M:list,b:list,order:list,n:int)->list:
 
     x = n * [0]
-    x[n - 1] = b[n - 1] / Matrix[n - 1][n - 1]
+    x[n - 1] = b[n - 1] / M[n - 1][n - 1]
 
     for i in range(n - 2, -1, -1):
         summ = 0
         for j in range(i + 1, n):
-            summ += Matrix[i][j] * x[j]
-        x[i] = (b[i] - summ) / Matrix[i][i]
+            summ += M[i][j] * x[j]
+        x[i] = (b[i] - summ) / M[i][i]
 
-    return x
+    return [x[i] for i in order]
 
 if __name__ == '__main__':
     Matrix = []
@@ -35,5 +72,6 @@ if __name__ == '__main__':
     b = list(map(int, f.read().split()))
     f.close()
 
-    x = GaussMethod(Matrix,b,n)
-    print(x)
+    #x = GaussMethod(Matrix.copy(),b.copy(),n)
+    y = UpgradedGaussMethod(Matrix.copy(),b.copy(),n)
+    print(y)
