7a89819d654ccabddda4be332ebef379b44c6767
nemec <cepuhin.semen@gmail.com>
2024-03-14 23:41:57 +0400

    v3

--- lab2/main.py
+++ lab2/main.py
@@ -1,94 +1,126 @@
 import copy
-def GaussMethod(M:list,b:list,n:int)->list:
+def GaussMethod(A:list,b:list,n:int)->list:
     for k in range(0, n):
         for i in range(k+1, n):
-            coefficient = M[i][k] / M[k][k]
+            coefficient = A[i][k] / A[k][k]
             for j in range(k, n):
                 if k == j:
-                    M[i][j] = 0
+                    A[i][j] = 0
                     continue
-                M[i][j] -= M[k][j] * coefficient
+                A[i][j] -= A[k][j] * coefficient
             b[i] -= b[k] * coefficient
 
     x = n * [0]
-    x[n - 1] = b[n - 1] / M[n - 1][n - 1]
+    x[n - 1] = b[n - 1] / A[n - 1][n - 1]
 
     for i in range(n - 2, -1, -1):
         summ = 0
         for j in range(i + 1, n):
-            summ += M[i][j] * x[j]
-        x[i] = (b[i] - summ) / M[i][i]
+            summ += A[i][j] * x[j]
+        x[i] = (b[i] - summ) / A[i][i]
 
     return x
 
-def UpgradedGaussMethod(M:list,b:list,n:int)->list:
+def UpgradedGaussMethod(A:list,b:list,n:int)->list:
     order = [i for i in range(n)]
 
     for k in range(0, n):
         column = k
         row = k
-        max_el = M[k][k]
+        max_el = A[k][k]
 
         #нахождение максимального элемента в матрице
         for i in range(k,n):
             for j in range(k,n):
-                if M[i][j] > max_el:
+                if A[i][j] > max_el:
                     row = i
                     column = j
-                    max_el = M[i][j]
+                    max_el = A[i][j]
 
         if k != row and k != column:
             # замена строк
-            M[k],M[row] = M[row],M[k]
+            A[k],A[row] = A[row],A[k]
             b[k],b[row] = b[row],b[k]
             # замена столбцов
             for i in range(n):
-                M[i][column],M[i][k] = M[i][k],M[i][column]
+                A[i][column],A[i][k] = A[i][k],A[i][column]
             order[k], order[column] = order[column], order[k]
 
         for i in range(k + 1, n):
-            coefficient = M[i][k] / M[k][k]
+            coefficient = A[i][k] / A[k][k]
             for j in range(k, n):
                 if k == j:
-                    M[i][j] = 0
+                    A[i][j] = 0
                     continue
-                M[i][j] -= M[k][j] * coefficient
+                A[i][j] -= A[k][j] * coefficient
             b[i] -= b[k] * coefficient
 
     x = n * [0]
-    x[n - 1] = b[n - 1] / M[n - 1][n - 1]
+    x[n - 1] = b[n - 1] / A[n - 1][n - 1]
 
     for i in range(n - 2, -1, -1):
         summ = 0
         for j in range(i + 1, n):
-            summ += M[i][j] * x[j]
-        x[i] = (b[i] - summ) / M[i][i]
+            summ += A[i][j] * x[j]
+        x[i] = (b[i] - summ) / A[i][i]
 
     return [x[i] for i in order]
 
 
-def Determinant(M:list,b:list,n:int):
+def Determinant(A:list,b:list,n:int):
     for k in range(0, n):
         for i in range(k+1, n):
-            coefficient = M[i][k] / M[k][k]
+            coefficient = A[i][k] / A[k][k]
             for j in range(k, n):
                 if k == j:
-                    M[i][j] = 0
+                    A[i][j] = 0
                     continue
-                M[i][j] -= M[k][j] * coefficient
+                A[i][j] -= A[k][j] * coefficient
             b[i] -= b[k] * coefficient
 
     det = 1
     for i in range(n):
-        det *= M[i][i]
+        det *= A[i][i]
 
     return det
 
+def ReverseMatrix(A:list,n:int):    
+
+    for i in range(n):
+        A[i]+= [0]*n
+        A[i][i+n] = 1
+    
+    for k in range(0, n):
+        for i in range(k+1, n):
+            coefficient = A[i][k] / A[k][k]
+            for j in range(k, 2*n):
+                if k == j:
+                    A[i][j] = 0
+                    continue
+                A[i][j] -= A[k][j] * coefficient
+            
+    Reverse = [n * [0] for _ in range(n)]
+
+    #k - столбец таблицы справа
+    for k in range(0,n):
+        x = n * [0]
+        x[n - 1] = A[n - 1][k+n] / A[n - 1][n - 1]
+        for i in range(n - 2, -1, -1):
+            summ = 0
+            for j in range(i + 1, n):
+                summ += A[i][j] * x[j]
+            x[i] = (A[i][k+n] - summ) / A[i][i]
+
+        for i in range(n):
+            Reverse[i][k] = x[i]
+
+    return Reverse
+    
 
 if __name__ == '__main__':
     A = []
 
-    f = open('lab2/matrix.txt', 'r')
+    f = open('lab2/matrix2.txt', 'r')
     n = int(f.readline())
     for i in range(n):
         line = f.readline()
@@ -100,3 +132,6 @@ if __name__ == '__main__':
     #rez = GaussMethod(copy.deepcopy(A),copy.deepcopy(b),n)
     #rez_upgrade = UpgradedGaussMethod(copy.deepcopy(A),copy.deepcopy(b),n)
     #det = Determinant(copy.deepcopy(A),copy.deepcopy(b),n)
+    rev = ReverseMatrix(copy.deepcopy(A),n)
+    for i in range(n):
+        print(rev[i])
